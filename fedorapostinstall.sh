3f9dbb953671f5fd970db741fc259a4a7265a0a7
ArcticStag <callum.bullard@me.com>
2020-09-09 20:55:05 +1000

    Added additional packages for initial install and auto select fastes mirrors

--- fedorapostinstall.sh
+++ fedorapostinstall.sh
@@ -6,29 +6,32 @@
 #   sudo dnf -y
 # }
 
+#Update the fastest mirror and deltarpm
+echo 'fastestmirror=true' >> /etc/dnf/dnf.conf
+echo 'deltarpm=true' >> /etc/dnf/dnf.conf
+sudo dnf clean all
+sudo dnf update --refresh
+
+# Install rpmfusion
 sudo dnf -y install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
 sudo dnf -y upgrade
 
+# Install Brave browser
 sudo dnf install dnf-plugins-core
-
 sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/x86_64/
-
 sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
 
-sudo dnf -y install brave-browser steam lutris git vim terminator neofetch htop
+# Install default applications
+sudo dnf -y install brave-browser steam lutris git vim terminator neofetch htop gimp
 
-cd ~/Downloads/
-
-wget https://github.com/Foundry376/Mailspring/releases/download/1.7.8/mailspring-1.7.8-0.1.x86_64.rpm
-wget https://atom-installer.github.com/v1.49.0/atom.x86_64.rpm
-
+# Attempt to install atom and mailspring
 sudo dnf -y install https://atom.io/download/rpm.rpm
 sudo dnf -y install https://updates.getmailspring.com/download?platform=linuxRpm.rpm
 
-rm -frv *.rpm
-
-cd ~/
-
+# Install keybase
 sudo dnf install -y  https://prerelease.keybase.io/keybase_amd64.rpm
 run_keybase
+
+sudo systemctl start sshd
+sudo systemctl enable sshd
